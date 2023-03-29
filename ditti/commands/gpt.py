9a89e485783db871809afbfe6579d90411c14624
alex paden <padenalex0@gmail.com>
2023-03-29 14:23:28 -0500

    rebuilt gpt command

--- ditti/commands/gpt.py
+++ ditti/commands/gpt.py
@@ -1,4 +1,5 @@
 import logging
+from collections import OrderedDict
 
 import openai
 from farcaster import Warpcast
@@ -7,17 +8,36 @@ from farcaster.models import Parent
 from ditti.commands.text2img import Text2Img
 
 
+class LimitedSizeDict(OrderedDict):
+    def __init__(self, *args, size_limit=None, **kwargs):
+        self.size_limit = size_limit
+        super().__init__(*args, **kwargs)
+
+    def __setitem__(self, key, value):
+        super().__setitem__(key, value)
+        self._check_size_limit()
+
+    def _check_size_limit(self):
+        if self.size_limit is not None:
+            while len(self) > self.size_limit:
+                self.popitem(last=False)
+
+
 class Gpt:
     def __init__(self, fcc: Warpcast, bot_username: str, gpt_com: str):
         self.fcc = fcc
         self.t2i = Text2Img()
         self.BOT_USERNAME = bot_username
         self.GPT_COM = gpt_com
+        self.cache = LimitedSizeDict(size_limit=100)
 
     # Will break if a parent is deleted, but that's a problem for another day
     def start_gpt(self, call_cast):
-        messages = self.get_messages_from_gpt_call(call_cast)
-        result = self.run_gpt_query(messages=messages)
+        messages, length_type = self.get_messages_from_gpt_call(call_cast)
+        if length_type == 0:
+            result = self.run_gpt_query(messages=messages)
+        else:
+            result = self.run_gpt_query_long(messages=messages)
         parent = Parent(fid=call_cast.author.fid, hash=call_cast.hash)
         return result, parent
 
@@ -30,11 +50,18 @@ class Gpt:
             if parent_cast.text.lower().startswith(
                 f"{self.BOT_USERNAME} {self.GPT_COM}"
             ):
-                og_messages = self.get_messages_from_gpt_call(parent_cast)
+                og_messages, length_type = self.get_messages_from_gpt_call(parent_cast)
                 messages[:0] = og_messages
                 break
             elif parent_cast.author.username == self.BOT_USERNAME[1:]:
-                messages.insert(0, {"role": "assistant", "content": parent_cast.text})
+                text = parent_cast.text
+                if text.startswith(
+                    "https://i.imgur.com/"
+                ):  # If the text is an imgur URL
+                    text = self.cache.get(
+                        text, text
+                    )  # Replace it with the cached text if available
+                messages.insert(0, {"role": "assistant", "content": text})
             else:
                 if parent_cast.parent_hash is not None:
                     temp_cast = self.fcc.get_cast(parent_cast.parent_hash).cast
@@ -56,10 +83,12 @@ class Gpt:
     def get_messages_from_gpt_call(self, call_cast):
         call_text = call_cast.text
         command_text = ""
+        length_type = 0
         parent_count = self.find_integer_after_caret(call_text)
         if parent_count is None:
             return "Something is wrong with your ^ command"
         if call_text.lower().startswith(f"{self.BOT_USERNAME} {self.GPT_COM}*"):
+            length_type = 1
             command_text = self.get_long_form_command(call_text, parent_count)
         elif call_text.lower().startswith(f"{self.BOT_USERNAME} {self.GPT_COM}^"):
             command_text = self.get_short_form_parent_command(call_text, parent_count)
@@ -80,9 +109,9 @@ class Gpt:
             counter += 1
 
         messages = self.simple_messages_string_builder(
-            parent_texts=parent_texts, query_text=query_text
+            parent_texts=parent_texts, query_text=query_text, length_type=length_type
         )
-        return messages
+        return messages, length_type
 
     # this finds how many parents to include in the query
     def find_integer_after_caret(self, s: str) -> int:
@@ -135,19 +164,33 @@ class Gpt:
         return command_text
 
     # This builds the messages list using parent texts as system input
-    def simple_messages_string_builder(self, parent_texts, query_text):
+    def simple_messages_string_builder(self, parent_texts, query_text, length_type):
         messages = []
         messages = [{"role": "system", "content": text} for text in parent_texts]
-        messages.insert(
-            0,
-            {
-                "role": "system",
-                "content": (
-                    "Given the initial system context, provide the shortest possible "
-                    "response to the user, ideally under 300 characters."
-                ),
-            },
-        )
+        if length_type == 0:
+            messages.insert(
+                0,
+                {
+                    "role": "system",
+                    "content": (
+                        "Given the initial system context, "
+                        "provide the shortest possible "
+                        "response to the user, ideally under 300 characters."
+                    ),
+                },
+            )
+        else:
+            messages.insert(
+                0,
+                {
+                    "role": "system",
+                    "content": (
+                        "Given the initial system context, "
+                        "provide a detailed response which "
+                        "best answers the users prompt."
+                    ),
+                },
+            )
         messages.append({"role": "user", "content": query_text})
         return messages
 
@@ -164,5 +207,24 @@ class Gpt:
 
         summary = response.choices[0].message.content
         if len(summary) > 320:
-            summary = self.t2i.convert(text=summary, color="white")
+            img_url = self.t2i.convert(text=summary)
+            self.cache[img_url] = summary  # Add the text message to the cache
+            summary = img_url
+        return summary
+
+    # Long form gpt query
+    def run_gpt_query_long(self, messages):
+        # Send the API request and get the response
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+            max_tokens=750,
+            temperature=0.65,
+        )
+
+        summary = response.choices[0].message.content
+        if len(summary) > 320:
+            img_url = self.t2i.convert(text=summary)
+            self.cache[img_url] = summary  # Add the text message to the cache
+            summary = img_url
         return summary
